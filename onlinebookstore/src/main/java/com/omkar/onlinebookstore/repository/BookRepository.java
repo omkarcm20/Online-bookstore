1244426c435a46ed923b38671886cfd44ca87c73
Omkar <omkarcm20@gmail.com>
2020-03-13 14:08:29 +0530

    created angular project and completed feature 1

--- onlinebookstore/src/main/java/com/omkar/onlinebookstore/repository/BookRepository.java
+++ onlinebookstore/src/main/java/com/omkar/onlinebookstore/repository/BookRepository.java
@@ -1,9 +1,11 @@
 package com.omkar.onlinebookstore.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.omkar.onlinebookstore.entity.Book;
 
+@CrossOrigin("http://localhost:4200")
 public interface BookRepository extends JpaRepository<Book, Long>{		//name of entity and type of primary key
 
 }
